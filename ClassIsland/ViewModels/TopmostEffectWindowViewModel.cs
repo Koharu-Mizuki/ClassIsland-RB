b0af891811ee133ef09ffbb3b0db9ceadb95a497
HPD HE <120182813+IsHPDuwu@users.noreply.github.com>
2026-03-29 11:03:52 +0800

    fix(notification): 在遮罩期间保持播放提醒音 #1704 #1673

---
+++ ClassIsland/ViewModels/TopmostEffectWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ClassIsland.ViewModels;
+
+public class TopmostEffectWindowViewModel : ObservableRecipient
+{
+    private ObservableCollection<Control> _effectControls = [];
+
+    public ObservableCollection<Control> EffectControls
+    {
+        get => _effectControls;
+        set
+        {
+            if (Equals(value, _effectControls)) return;
+            _effectControls = value;
+            OnPropertyChanged();
+        }
+    }
+}
